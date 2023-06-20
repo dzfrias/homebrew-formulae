6671f3e384c821c11b6ac54c57eda5c26d0e0c0b
Diego <96022404+dzfrias@users.noreply.github.com>
2023-06-20 09:35:10 -0700

    style(projectable): remove redundant version

--- Formula/projectable.rb
+++ Formula/projectable.rb
@@ -2,7 +2,6 @@ class Projectable < Formula
   desc "Highly configurable TUI project manager"
   homepage "https://github.com/dzfrias/projectable"
   url "https://github.com/dzfrias/projectable/archive/refs/tags/0.2.0.tar.gz"
-  version "0.2.0"
   sha256 "58ddd8f9b1ad2ece9e4718963344092a69d1fe62008ede148fa99eea6e8cea2c"
 
   depends_on "rust" => :build
