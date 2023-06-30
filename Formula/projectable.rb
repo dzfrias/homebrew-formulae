611c59771fd8fd23d098f9542016ada13292f513
Diego <96022404+dzfrias@users.noreply.github.com>
2023-06-30 13:32:53 -0700

    feat(projectable): update to v1.3.0

--- Formula/projectable.rb
+++ Formula/projectable.rb
@@ -1,8 +1,8 @@
 class Projectable < Formula
   desc "Highly configurable TUI project manager"
   homepage "https://github.com/dzfrias/projectable"
-  url "https://github.com/dzfrias/projectable/archive/refs/tags/1.2.0.tar.gz"
-  sha256 "7cc43db900f016219ca8e742f2f34d7d0389130fcc240d67ab50547a8d268f31"
+  url "https://github.com/dzfrias/projectable/archive/refs/tags/1.3.0.tar.gz"
+  sha256 "fe1c0edf9f14f2cd9cfef7e9af921f3e4b307b5c518a7b79f96563d6269a1e72"
 
   depends_on "rust" => :build
 
